15cb890d5d011ca2948a800fe4291b8dce934226
Yvon Viger <yvon@baytek.ca>
2017-07-21 11:42:31 -0400

    Fixing namespace issue

--- src/Controllers/Auth/LoginController.php
+++ src/Controllers/Auth/LoginController.php
@@ -29,7 +29,7 @@ class LoginController extends Controller
      */
     public function showLoginForm()
     {
-        return view('User::login');
+        return view('user::login');
     }
 
     /**
